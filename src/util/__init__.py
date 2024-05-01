3b1863c14b1c5065636d283d9afb2afe3d4ccd58
David Irwin <david.irwin@interfolio.com>
2024-05-01 13:09:14 -0400

    Create base url subclass of requests session

---
+++ src/util/__init__.py
@@ -0,0 +1,1 @@
+from .extended_request import ExtendedRequest
---
+++ src/util/extended_request.py
@@ -0,0 +1,10 @@
+from requests import Session
+from urllib.parse import urljoin
+class ExtendedRequest(Session):
+    def __init__(self, base_url=None):
+        super().__init__()
+        self.base_url = base_url
+
+    def request(self, method, url, *args, **kwargs):
+        url = urljoin(self.base_url, url)
+        return super().request(method, url, *args, **kwargs)
